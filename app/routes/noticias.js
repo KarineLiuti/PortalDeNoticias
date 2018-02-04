047213e0a85614ada7700da01acdfb025eee9488
Karine Liuti <kah_liuti@hotmail.com>
2018-02-04 18:23:53 -0200

    Configurado banco de dados MySQL; Criado consulta para portal de notícias.

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,19 @@
 module.exports = (app) => {
+    
     app.get('/noticias', (req, res) => {
-        res.render('noticias/noticias');
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'portal_noticias'
+    });
+    
+    connection.query('select * from noticias', (erro, result) => {
+        res.send(result);
+    })
+    
+    //res.render('noticias/noticias');
     });
 };
