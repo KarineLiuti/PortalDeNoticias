c4b8e356b36c547268fec5e6fff485ef375de1f5
Karine Liuti <kah_liuti@hotmail.com>
2018-02-04 19:13:51 -0200

    Inclusão do módulo Consign.

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var app = require('./config/server');
 
-var rotaHome = require('./app/routes/home')(app);
-var rotaNoticias = require('./app/routes/noticias')(app);
-var rotaFormIncusaoNoticia = require('./app/routes/form_add_noticia')(app);
-
 app.listen(3000, (req, res) => {
     console.log('Server ON');
 });
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,18 +1,12 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = (app) => {
-    
+
     app.get('/noticias', (req, res) => {
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'root',
-            database: 'portal_noticias'
+        var connection = dbConnection();
+            
+        connection.query('select * from noticias', (erro, result) => {
+            res.render('noticias/noticias', {noticias : result});
         });
-    
-    connection.query('select * from noticias', (erro, result) => {
-        res.render('noticias/noticias', {noticias : result});
-    });
-
     });
 };
