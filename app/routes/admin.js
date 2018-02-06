28269f4305465db6ae33f710db9b76a541c09d9d
Karine Liuti <karine.silva@inflor.com.br>
2018-02-06 13:00:45 -0200

    S8-A37 - Inclusão de formulário e método POST.

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,4 +2,11 @@ module.exports = (app) => {
     app.get('/formulario_inclusao_noticia', (req, res) => {
         res.render('admin/form_add_noticia');
     });    
+
+    app.post('/noticias/salvar', (req, res) => {
+        res.send('chegou na página');
+    });   
+
+
+    
 }
