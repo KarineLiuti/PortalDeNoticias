eac18ce91dd8d8d1150ca5baa0cc971384860d50
Karine Liuti <karine.silva@inflor.com.br>
2018-02-07 07:59:07 -0200

    Inserido método para salvar informações do formulário de novas notícias no banco de dados.

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,17 @@
-module.exports = (app) => {
-    app.get('/formulario_inclusao_noticia', (req, res) => {
+module.exports = (application) => {
+    application.get('/formulario_inclusao_noticia', (req, res) => {
         res.render('admin/form_add_noticia');
     });    
 
-    app.post('/noticias/salvar', (req, res) => {
-        var noticias = req.body;
-        res.send(noticias);
+    application.post('/noticias/salvar', (req, res) => {
+        var noticia = req.body;
+
+        var connection = application.config.dbConnection();
+        var noticiasModel = application.app.models.noticiasModel;
+
+        noticiasModel.salvarNoticia(noticia, connection, (erro, result) => {
+            res.redirect('/noticias');
+        });
+        
     });   
 }
